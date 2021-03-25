58f95ea02ae06229ad105da86bcfea73163f621b
joe hobson <joe@navigationnorth.com>
2021-03-25 07:33:18 -0600

    modifications for passenger deploy see: https://github.com/instructure/canvas-rce-api/issues/12

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-"use strict";
+const path = require("path");
 
-require("dotenv").config();
-// unsplash-js requires fetch to be available in the global scope
-// see: https://github.com/unsplash/unsplash-js#adding-polyfills
+require("dotenv").config({
+  path: path.join(__dirname, ".env")
+});
 global.fetch = require("node-fetch");
 
 const container = require("./app/container");
--- app/application.js
+++ app/application.js
@@ -6,6 +6,10 @@ const withMiddleware = require("./middleware");
 const _env = require("./env");
 const _routes = require("./routes");
 
+if (typeof PhusionPassenger !== "undefined") {
+  PhusionPassenger.configure({ autoInstall: false });
+}
+
 function inject(provide) {
   return [_env, _routes, provide(console), provide(express()), _stats];
 }
@@ -16,8 +20,14 @@ function init(env, routes, logger, app, stats) {
   const port = env.get("PORT", () => 3000);
   return {
     listen() {
-      const server = app.listen(port);
-      logger.log(`Rich Content Service listening on port ${port}`);
+      var server = null;
+      if (typeof PhusionPassenger !== "undefined") {
+        logger.log("we are running in phusion");
+        server = app.listen("passenger");
+      } else {
+        logger.log("we are running outside of phusion, on port " + port);
+        server = app.listen(port);
+      }
       return server;
     }
   };
